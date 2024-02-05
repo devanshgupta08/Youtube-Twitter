85cb14e6670b055ac0142a021d23893f728cf3c7
Devansh Gupta <devansh00gupta@gmail.com>
2024-02-05 12:45:41 +0530

    utils created

--- src/db/connection.js
+++ src/db/connection.js
@@ -5,7 +5,7 @@ const connectDB = async ()=>{
     try
     {   
         const connectionInstance =  await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB connected  DB HOST: ${connectionInstance.connection.host}`);
+        console.log(`\nMongoDB connected  DB HOST: ${connectionInstance.connection.host}`);
     }
     catch(error)
     {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connection.js"
+import {app} from "./app.js"
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(err) =>{
+        console.log("Express Error",err);
+    })
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`Listning on port ${process.env.PORT || 3000}`);
+    })
+})
+.catch((err) =>{
+    console.log("Mongo connection error ",err);
+})
 
